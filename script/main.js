52d1832c95609399ffc25c91894217c5f3d8cc75
Lionel O <lionel.orsini@hotmail.fr>
2017-10-04 12:48:30 +0200

    add js to that . part 1

---
+++ script/main.js
@@ -0,0 +1,55 @@
+console.log('ok bien loaded');
+
+
+// variables
+const appearAfter = document.querySelectorAll('.appearAfter'),
+      button = document.querySelector('#clickToShow button'),
+      launchSearchButton = document.getElementById('launchSearch'),
+      input = document.querySelector('#query > input'),
+      form = document.getElementById('query');
+
+
+// fonctions
+const activateContent = () => {
+  const containerToHide = document.getElementById('clickToShow'),
+        cssValues = window.getComputedStyle(containerToHide);
+
+  let delay = cssValues.getPropertyValue('transition-duration');
+
+  delay = delay.replace('s', '') * 1000 + 50;
+
+  button.classList.add('unopacity');
+  setTimeout(() => {
+    containerToHide.style.cssText = `opacity : 0;`;
+  }, delay);
+
+  setTimeout(() => {
+    containerToHide.style.cssText = `display: none;`;
+  }, delay * 2);
+}
+
+const launchQuery = () => {
+  let query = document.querySelector('#query > input').value;
+  query = query.replace(' ', '+');
+  window.location = `http://www.google.fr/search?q=${query}`;
+}
+
+const snitchEnterKey = e => {
+  console.log(e.keyCode);
+  if (e.keyCode === 13) {
+    launchQuery();
+  }
+}
+
+const noSub = e => {
+  e.preventDefault();
+}
+
+
+// events
+button.addEventListener('click', activateContent);
+launchSearchButton.addEventListener('click', launchQuery);
+input.addEventListener('keypress', snitchEnterKey);
+form.addEventListener('submit', noSub);
+
+
